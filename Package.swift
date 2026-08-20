09ba74c7d43af94b072cd471d0fe5269ac9f78c4
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:08:17 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-predicate-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -22,13 +22,16 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-logic-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-logic-primitives.git",
+            branch: "main"
+        )
     ],
     targets: [
         .target(
             name: "Predicate Primitives",
             dependencies: [
-                .product(name: "Logic Ternary Primitives", package: "swift-logic-primitives"),
+                .product(name: "Logic Ternary Primitives", package: "swift-logic-primitives")
             ]
         ),
         .target(
